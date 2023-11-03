35631d637be0fc9a57cba883210b3d33fa8b1ad8
kenedy <kipkiruikenedy@gmail.com>
2023-11-03 16:48:40 +0300

    new

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -5,52 +5,52 @@
     <h2 class="text-xl font-semibold">2021 AGM Photos</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div>
-        <img src="link-to-2021-agm-photo-1.jpg" alt="2021 AGM Photo 1" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2021 AGM Photo 1" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2021-agm-photo-2.jpg" alt="2021 AGM Photo 2" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2021 AGM Photo 2" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2021-agm-photo-3.jpg" alt="2021 AGM Photo 3" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2021 AGM Photo 3" class="rounded-lg" />
       </div>
     </div>
 
     <h2 class="text-xl font-semibold mt-6">2022 AGM Photos</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div>
-        <img src="link-to-2022-agm-photo-1.jpg" alt="2022 AGM Photo 1" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2022 AGM Photo 1" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2022-agm-photo-2.jpg" alt="2022 AGM Photo 2" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2022 AGM Photo 2" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2022-agm-photo-3.jpg" alt="2022 AGM Photo 3" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2022 AGM Photo 3" class="rounded-lg" />
       </div>
     </div>
 
     <h2 class="text-xl font-semibold mt-6">2023 AGM Photos</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div>
-        <img src="link-to-2023-agm-photo-1.jpg" alt="2023 AGM Photo 1" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2023 AGM Photo 1" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2023-agm-photo-2.jpg" alt="2023 AGM Photo 2" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2023 AGM Photo 2" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-2023-agm-photo-3.jpg" alt="2023 AGM Photo 3" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="2023 AGM Photo 3" class="rounded-lg" />
       </div>
     </div>
 
     <h2 class="text-xl font-semibold mt-6">BOD Member Retreat 2023</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div>
-        <img src="link-to-bod-retreat-photo-1.jpg" alt="BOD Member Retreat 2023 Photo 1" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="BOD Member Retreat 2023 Photo 1" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-bod-retreat-photo-2.jpg" alt="BOD Member Retreat 2023 Photo 2" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="BOD Member Retreat 2023 Photo 2" class="rounded-lg" />
       </div>
       <div>
-        <img src="link-to-bod-retreat-photo-3.jpg" alt="BOD Member Retreat 2023 Photo 3" class="rounded-lg" />
+        <img src="../assets/images/agm.jpg" alt="BOD Member Retreat 2023 Photo 3" class="rounded-lg" />
       </div>
     </div>
   </div>
