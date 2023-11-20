558a753093665c2c61e5c002e86ed28fd7c8851c
kenedy <kipkiruikenedy@gmail.com>
2023-11-20 08:20:53 +0300

    new

--- src/pages/LoanFeatures.vue
+++ src/pages/LoanFeatures.vue
@@ -8,7 +8,11 @@
             <h2 class="text-xl font-semibold">{{ loan.name }}</h2>
             <i :class="loan.open ? 'fas fa-chevron-up' : 'fas fa-chevron-down'"></i>
           </div>
-          <div v-if="loan.open" class="mt-4 text-gray-600">{{ loan.description }}</div>
+          <div v-if="loan.open" class="mt-4 text-gray-600">
+            <ul>
+              <li v-for="point in loan.description" :key="point"> <i class="fas fa-check mr-1"></i><span class="ml-1">{{ point }}</span></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -22,115 +26,127 @@ const loans = ref([
   {
     id: 1,
     name: 'Ufanisi Loan',
-    description: `
-      Maximum three times a member’s deposits, subject to a ceiling of Kshs 7.5 million, or an amount set by the Board of Directors.
-      Interest shall be charged at 1% monthly on reducing balance and may be varied from time to time by the BOD.
-      The maximum repayment period shall be 60 months which may be varied by the Board of Directors from time to time as the cash flow dictates.
-    `,
+    description: [
+      'Maximum three times a member’s deposits, subject to a ceiling of Kshs 7.5 million, or an amount set by the Board of Directors.',
+      'Interest shall be charged at 1% monthly on reducing balance and may be varied from time to time by the BOD.',
+      'The maximum repayment period shall be 60 months which may be varied by the Board of Directors from time to time as the cash flow dictates.',
+    ],
     open: false,
   },
   {
     id: 2,
     name: 'Asset Finance Loan / Product Loan',
-    description: `
-    For purchase of other products, household goods, vehicles or land.
-
-Interest rate shall be charged at 12% p.a or any other interest to be determined by the Board of Directors from time to time.
-Members to apply for in the normal terms.
-Repayment period is 60 months.
-Maximum amount is Kshs.10million.
-Applicable to members with deposits of at least 15% of the loan applied.
-    `,
+    description: [
+      'For purchase of other products, household goods, vehicles or land.',
+      'Interest rate shall be charged at 12% p.a or any other interest to be determined by the Board of Directors from time to time.',
+      'Members to apply for in the normal terms.',
+      'Repayment period is 60 months.',
+      'Maximum amount is Kshs.10million.',
+      'Applicable to members with deposits of at least 15% of the loan applied.',
+    ],
     open: false,
   },
   {
     id: 3,
     name: 'Refinancing Loan',
-    description:`Members to apply for the loan under the normal terms.
-Member to apply after at least 6 months of servicing the current loan.
-Interest shall be charged at 12% p.a on reducing balance and may be varied from time to time by the BOD.
-These loans are provided depending on the availability of funds in the Society and on a first come first served basis.
-The refinancing fee shall be 1% on the outstanding balance.
-Maximum amount should not exceed Kshs. 7.5 million or an amount set by BOD.
-Maximum repayment period shall be 60 months.`,
+    description: [
+      'Members to apply for the loan under the normal terms.',
+      'Member to apply after at least 6 months of servicing the current loan.',
+      'Interest shall be charged at 12% p.a on reducing balance and may be varied from time to time by the BOD.',
+      'These loans are provided depending on the availability of funds in the Society and on a first come first served basis.',
+      'The refinancing fee shall be 1% on the outstanding balance.',
+      'Maximum amount should not exceed Kshs. 7.5 million or an amount set by BOD.',
+      'Maximum repayment period shall be 60 months.',
+    ],
     open: false,
   },
   {
     id: 4,
     name: 'Daraja Loan',
-    description: `Maximum amount shall be Kshs 400,000.00 or an amount set by the BOD.
-Interest shall be charged at 13% pa on reducing balance or a rate set by BOD.
-Maximum repayment period shall be 24 months.`,
+    description: [
+      'Maximum amount shall be Kshs 400,000.00 or an amount set by the BOD.',
+      'Interest shall be charged at 13% pa on reducing balance or a rate set by BOD.',
+      'Maximum repayment period shall be 24 months.',
+    ],
     open: false,
   },
   {
     id: 5,
     name: 'M-Biblia Loan',
-    description: ` This is a mobile banking facility that allows members the opportunity to access loans via their phones.
-
-Maximum loan of 50,000 but to be graduated from Ksh. 5,000.00.
-Interest rate of 5% per month on reducing balance.
-Repayment period up to a maximum of 3 months.
-The monthly repayment is facilitated by the member.
-The member can access up to 80% of his free deposits.
-In case of default the loan balance is recovered from the members’ deposit.`,
+    description: [
+      'This is a mobile banking facility that allows members the opportunity to access loans via their phones.',
+      'Maximum loan of 50,000 but to be graduated from Ksh. 5,000.00.',
+      'Interest rate of 5% per month on reducing balance.',
+      'Repayment period up to a maximum of 3 months.',
+      'The monthly repayment is facilitated by the member.',
+      'The member can access up to 80% of his free deposits.',
+      'In case of default the loan balance is recovered from the members’ deposit.',
+    ],
     open: false,
   },
   {
     id: 6,
     name: 'Insurance Premium Loan',
-    description: `Members to apply for the loan under the normal terms.
-Member must be contributing deposits and repaying their loans consistently.
-Repayment period 12 months.
-Interest shall be charged at 13% per annum on reducing balance.
-Maximum subject to premium payable.
-Cheque is payable directly to the insurance company.`,
+    description: [
+      'Members to apply for the loan under the normal terms.',
+      'Member must be contributing deposits and repaying their loans consistently.',
+      'Repayment period 12 months.',
+      'Interest shall be charged at 13% per annum on reducing balance.',
+      'Maximum subject to premium payable.',
+      'Cheque is payable directly to the insurance company.',
+    ],
     open: false,
   },
   {
     id: 7,
     name: 'Elimu Loan',
-    description:`Maximum amount shall be Kshs. 500,000.00 or an amount set by BOD.
-Interest shall be charged at 12% p.a on reducing balance and may be varied from time to time by the BOD.
-Maximum repayment period shall be 24 months.
-Application for school fees loan should be submitted together with a signed and stamped fee structure.`,
+    description: [
+      'Maximum amount shall be Kshs. 500,000.00 or an amount set by BOD.',
+      'Interest shall be charged at 12% p.a on reducing balance and may be varied from time to time by the BOD.',
+      'Maximum repayment period shall be 24 months.',
+      'Application for school fees loan should be submitted together with a signed and stamped fee structure.',
+    ],
     open: false,
   },
   {
     id: 8,
     name: 'Makao Poa Loan',
-    description: `This product is designed to help members in their investment and development plans e.g.acquire a plot, renovate, finish or even construct commercial, rental and residential properties.
-Minimum loan shall be Kshs. 1million and Maximum loan amount shall be Kshs.10 million subject to ability to repay.
-Interest rate charged on the loan is 12% p.a. on reducing balance.
-Maximum repayment period of 72 months.
-The loan must be fully secured by guarantors and/or collateral.
-Member must have a good credit history and ability to repay.
-This loan shall not be used to refinance any other loan.
-Applicable to members with deposits of at least 15% of the loan applied`,
+    description: [
+      'This product is designed to help members in their investment and development plans e.g.acquire a plot, renovate, finish or even construct commercial, rental and residential properties.',
+      'Minimum loan shall be Kshs. 1million and Maximum loan amount shall be Kshs.10 million subject to ability to repay.',
+      'Interest rate charged on the loan is 12% p.a. on reducing balance.',
+      'Maximum repayment period of 72 months.',
+      'The loan must be fully secured by guarantors and/or collateral.',
+      'Member must have a good credit history and ability to repay.',
+      'This loan shall not be used to refinance any other loan.',
+      'Applicable to members with deposits of at least 15% of the loan applied.',
+    ],
     open: false,
   },
   {
     id: 9,
     name: 'Upesi Loan',
-    description:`Maximum amount shall be Kshs. 300,000.00, or an amount determined by the BOD from time to time.
-Interest shall be charged at 14% p.a on reducing balance and may be varied from time to time by the BOD.
-Maximum repayment period shall be 24 months.
-The applicant will be required to state reasons for emergency`,
+    description: [
+      'Maximum amount shall be Kshs. 300,000.00, or an amount determined by the BOD from time to time.',
+      'Interest shall be charged at 14% p.a on reducing balance and may be varied from time to time by the BOD.',
+      'Maximum repayment period shall be 24 months.',
+      'The applicant will be required to state reasons for emergency',
+    ],
     open: false,
   },
   {
     id: 10,
     name: 'Digitech Loan',
-    description: `
-    The Sacco will finance at 1.2% per month for a period of up to 12 months.
-Maximum amount shall be Kshs. 100,000.
-The loan should be fully secured by guarantors.
-Items to be purchased include but not limited to; Tank, office furniture, Electronic appliances e.g Mobile, Television, Laptops.
-Partnerships with relevant suppliers to be initiated e.g. safaricom, Hotpoint, top tank
-    `,
+    description: [
+      'The Sacco will finance at 1.2% per month for a period of up to 12 months.',
+      'Maximum amount shall be Kshs. 100,000.',
+      'The loan should be fully secured by guarantors.',
+      'Items to be purchased include but not limited to; Tank, office furniture, Electronic appliances e.g Mobile, Television, Laptops.',
+      'Partnerships with relevant suppliers to be initiated e.g. safaricom, Hotpoint, top tank',
+    ],
     open: false,
   },
-  ]);
+]);
 
 const toggleDescription = (loanId) => {
   const loan = loans.value.find((l) => l.id === loanId);
--- src/pages/membersupport/Signin.vue
+++ src/pages/membersupport/Signin.vue
@@ -1,98 +1,54 @@
 <template>
-  <div class="member-portal-container">
-    <div class="member-portal">
-      <div class="content">
-        <div class="text-container">
-          <h1 class="text-4xl font-bold mb-6 text-primary">How to Sign in to the Member Portal</h1>
+  <div class="container mx-auto mt-8 flex flex-col lg:flex-row items-center justify-between">
+    <!-- Content Section -->
+    <div class="lg:w-1/2 pr-8">
+      <h1 class="text-3xl font-semibold mb-4">How to Sign in to the Member Portal</h1>
+      <p class="text-gray-800">
+        Before we register you, it is important we have key information from you. That information will help in smooth registration.
+        Fill it and submit it either physically or through email. Now how to register;
+      </p>
+      <ol class="list-decimal pl-6 mt-4">
+        <li>Open any browser on your phone or computer.</li>
+        <li>On the Tab Type <strong>portal.bibliasacco.com</strong></li>
+        <li>A sign-in page will appear.</li>
+        <li>Enter your Full Membership number e.g., ASK01234.</li>
+        <li>Enter a 4-digit password sent to your phone as an SMS (NB: 4-digit password is only sent to the phone number that one provided when filling the form). This is a one-time password.</li>
+        <li>You will be prompted with Captcha verification (Please enter the captcha text above) and click validate.</li>
+        <li>Then you will be prompted with OTP verification code (Please enter the one-time password received via SMS) sent to your phone (NB: OTP verification code is only sent to the phone number that one provided when filling the form).</li>
+        <li>Enter the 6-digit OTP verification code.</li>
+        <li>Click Validate when prompted with KYC Verification.</li>
+        <li>If it's your first time, you will be required to enter a new password and confirm it.</li>
+        <li>You can now Login using your membership number and the password you have just created.</li>
+        <li>Done.</li>
+      </ol>
+    </div>
 
-          <p class="text-gray-700 mb-6">
-            Before we register you, it is important to have key information from you. This information will help in a smooth registration process. Fill it and submit it either physically or through email. Now, here's how to register:
-          </p>
-
-          <ol class="list-decimal text-gray-700 mb-8">
-            <li>Open any browser on your phone or computer.</li>
-            <li>Visit <span class="font-semibold">portal.bibliasacco.com</span>.</li>
-            <li>A sign-in page will appear.</li>
-            <li>Enter your Full Membership number (e.g., ASK01234).</li>
-            <li>Enter the 4-digit password sent to your phone as an SMS (one-time password).</li>
-            <li>Complete the Captcha verification and click validate.</li>
-            <li>Enter the OTP verification code sent to your phone.</li>
-            <li>Enter the 6-digit OTP verification code.</li>
-            <li>Click Validate when prompted with KYC Verification.</li>
-            <li>If it's your first time, set a new password.</li>
-            <li>Login using your membership number and the new password.</li>
-          </ol>
-
-          <p class="text-green-500 font-semibold">Done!</p>
-        </div>
-      </div>
-
-      <div class="image-container">
-        <!-- Add your sign-in image here -->
-        <img src="../../assets/images/signin.jpg" alt="Sign In" class="w-full h-auto rounded-lg shadow-lg">
-      </div>
+    <!-- Image Section -->
+    <div class="lg:w-1/2 lg:pl-8 lg:mt-0 mt-8">
+      <img src="../../assets/images/signin.jpg" alt="Member Portal" class="w-full max-w-md rounded-md shadow-lg lg:float-right"/>
     </div>
   </div>
 </template>
 
 <script setup>
-// You can add any necessary logic here
+// You can add script setup if needed
 </script>
 
 <style scoped>
-.member-portal-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
 }
 
-.member-portal {
-  max-width: 95vw;
-  width: 100%;
-  padding: 20px; /* Adjust padding for smaller screens */
-  border-radius: 20px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-  display: flex;
-  flex-direction: column; /* Adjust layout for smaller screens */
-}
-
-.content {
-  flex: 1;
-}
-
-.text-container {
-  max-width: 100%; /* Adjust max-width for smaller screens */
-}
-
-.image-container {
-  flex: 1;
-  margin-top: 20px; /* Adjust margin for smaller screens */
-}
-
-.text-primary {
-  color: #3498db; /* You can replace this color with your brand color */
-}
-
-.text-green-500 {
-  color: #27ae60; /* You can replace this color with your brand color */
-}
-
-.font-semibold {
-  font-weight: 600;
-}
-
-/* Add any additional styling here */
-
-@media screen and (max-width: 768px) {
-  /* Adjust styles for screens with a maximum width of 768 pixels */
-  .member-portal {
-    padding: 10px;
+/* Add any additional styling for responsiveness */
+@media (max-width: 1024px) {
+  .container {
+    padding: 0 10px;
   }
 
-  .image-container {
-    margin-top: 10px;
+  img {
+    margin: 0 auto;
   }
 }
 </style>
