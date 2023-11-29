c1f375e7dede3f3ad43fd0a48bc47d94cb96a3ca
kenedy <kipkiruikenedy@gmail.com>
2023-11-29 12:58:36 +0300

    new

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,62 +1,42 @@
 <template>
-  <div class="bg-gray-100 p-4">
-    <h1 class="text-4xl font-bold text-center mb-8">Biblia Gallery</h1>
-
-    <div v-for="(album, index) in albums" :key="index" class="mb-8">
-      <h2 class="text-2xl font-semibold mb-4">{{ album.title }}</h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div v-for="photo in album.photos" :key="photo.id" class="rounded-lg overflow-hidden">
-          <img :src="photo.src" :alt="photo.alt" class="w-full h-auto" />
+  <div class="min-h-screen bg-gray-100">
+    <div class="text-center py-8">
+      <h1 class="text-4xl font-bold text-gray-800">Bibilia Sacco Gallery</h1>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <!-- Image 1 -->
+      <div class="rounded-lg overflow-hidden shadow-lg">
+        <img src="../assets/images/agm.jpg" alt="Bibilia Sacco Office" class="w-full h-auto">
+        <div class="p-4">
+          <p class="text-gray-800 font-bold text-lg">Bibilia Sacco Office</p>
+          <p class="text-gray-600">Our main office building.</p>
         </div>
       </div>
+
+      <!-- Image 2 -->
+      <div class="rounded-lg overflow-hidden shadow-lg">
+        <img src="../assets/images/agm.jpg" alt="Bibilia Sacco Members Meeting" class="w-full h-auto">
+        <div class="p-4">
+          <p class="text-gray-800 font-bold text-lg">Members Meeting</p>
+          <p class="text-gray-600">A gathering of Bibilia Sacco members.</p>
+        </div>
+      </div>
+
+      <!-- Image 3 -->
+      <div class="rounded-lg overflow-hidden shadow-lg">
+        <img src="../assets/images/members.png" alt="Bibilia Sacco Savings Day" class="w-full h-auto">
+        <div class="p-4">
+          <p class="text-gray-800 font-bold text-lg">Savings Day Event</p>
+          <p class="text-gray-600">Celebrating members' savings achievements.</p>
+        </div>
+      </div>
+
+      <!-- Add more images as needed -->
+
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'BibliaGallery',
-  data() {
-    return {
-      albums: [
-        {
-          title: '2021 AGM Photos',
-          photos: [
-            { id: 1, src: '../assets/images/agm1.jpg', alt: 'Photo 1' },
-            { id: 2, src: '../assets/images/agm2.jpg', alt: 'Photo 2' },
-            { id: 3, src: '../assets/images/agm3.jpg', alt: 'Photo 3' },
-          ],
-        },
-        // Add other albums and photos here
-      ],
-    };
-  },
-};
-</script>
-
 <style scoped>
-/* Add Tailwind CSS classes and custom styles here */
-
-/* Style the image containers */
-.rounded-lg {
-  border-radius: 0.75rem;
-  overflow: hidden;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-/* Style the images to maintain aspect ratio */
-img {
-  width: 100%;
-  height: auto;
-}
-
-/* Style the section headers */
-h2 {
-  color: #1a202c;
-}
-
-/* Center the page title */
-h1 {
-  text-align: center;
-}
+/* Add Tailwind CSS classes as needed */
 </style>
